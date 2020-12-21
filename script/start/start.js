e06356f9df4c0e063b8f8275cf80433d56853f92
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-21 09:13:37 +0100

    what about listening every ip ?

--- script/start/start.js
+++ script/start/start.js
@@ -16,7 +16,6 @@ export const serverPromise = startServer({
   logLevel: process.env.LOG_LEVEL || "info",
   protocol: process.env.HTTPS ? "https" : "http",
   http2: Boolean(process.env.HTTPS),
-  ip: "",
   port: process.env.PORT || 0,
   requestToResponse: (request) => {
     if (request.ressource === "/") {
