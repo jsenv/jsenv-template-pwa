f7fc596d1bf07925b0425e5f935af928485f29db
Damien Maillard <damien.maillard@dailymotion.com>
2021-10-05 16:27:15 +0200

    update babel config file

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,21 @@
+/*
+ * This file is used to configure a list of babel plugins as documented in
+ * https://babeljs.io/docs/en/config-files
+ *
+ * During dev: babel plugins natively supported by browsers and Node.js are not used.
+ * During build:
+ *  - When "runtimeSupport" is configured, babel plugins already supported by these runtime won't be used
+ *  See https://github.com/jsenv/jsenv-template-pwa/blob/main/jsenv.config.mjs#L12
+ *  - Otherwise all babel plugins are use
+ *
+ */
+
+// "@babel/preset-env" transforms async function to generators
+// but it's verbose and slow compared to using promises, so we:
+// 1. Exclude "transform-async-to-generator", "transform-regenerator"
+// 2. Enable "babel-plugin-transform-async-to-promises"
+// See https://github.com/babel/babel/issues/8121
+
 module.exports = {
   presets: [
     [
