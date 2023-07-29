7c812350eded9af66d07393f9a83177125f75357
dmail <dmaillard06@gmail.com>
2023-07-29 10:26:48 +0000

    Deploying to gh-pages from @ jsenv/jsenv-template-pwa@6ac888828507ae69a61524f4f3fd2cb062d65afe 🚀

--- service_worker.js
+++ service_worker.js
@@ -1,26 +1,13 @@
-
 self.resourcesFromJsenvBuild = {
   "/jsenv-template-pwa/index.html": {
-    "version": "5548f2e3"
+    "version": "4eb64167"
   },
   "/jsenv-template-pwa/other/favicon.png": {
     "version": "467b6542",
     "versionedUrl": "/jsenv-template-pwa/other/favicon.png?v=467b6542"
   },
   "/jsenv-template-pwa/other/pwa.webmanifest": {
-    "version": "afbaade4"
-  },
-  "/jsenv-template-pwa/other/logo.png": {
-    "version": "467b6542",
-    "versionedUrl": "/jsenv-template-pwa/other/logo.png?v=467b6542"
-  },
-  "/jsenv-template-pwa/css/app.css": {
-    "version": "df3f7a57",
-    "versionedUrl": "/jsenv-template-pwa/css/app.css?v=df3f7a57"
-  },
-  "/jsenv-template-pwa/js/main.nomodule.js": {
-    "version": "814901e3",
-    "versionedUrl": "/jsenv-template-pwa/js/main.nomodule.js?v=814901e3"
+    "version": "ef7175da"
   },
   "/jsenv-template-pwa/other/pwa_icon_192.png": {
     "version": "eece115e",
@@ -34,25 +21,37 @@ self.resourcesFromJsenvBuild = {
     "version": "33ca6666",
     "versionedUrl": "/jsenv-template-pwa/other/maskable_icon_192.png?v=33ca6666"
   },
-  "/jsenv-template-pwa/js/app.nomodule.js": {
-    "version": "658dcef8",
-    "versionedUrl": "/jsenv-template-pwa/js/app.nomodule.js?v=658dcef8"
+  "/jsenv-template-pwa/other/logo.png": {
+    "version": "467b6542",
+    "versionedUrl": "/jsenv-template-pwa/other/logo.png?v=467b6542"
   },
-  "/jsenv-template-pwa/js/vendors.nomodule.js": {
-    "version": "39bb75dc",
-    "versionedUrl": "/jsenv-template-pwa/js/vendors.nomodule.js?v=39bb75dc"
+  "/jsenv-template-pwa/js/main.nomodule.js": {
+    "version": "71f0c98e",
+    "versionedUrl": "/jsenv-template-pwa/js/main.nomodule.js?v=71f0c98e"
   },
   "/jsenv-template-pwa/other/roboto_v27_latin_regular.woff2": {
     "version": "fda7bb73",
     "versionedUrl": "/jsenv-template-pwa/other/roboto_v27_latin_regular.woff2?v=fda7bb73"
   },
+  "/jsenv-template-pwa/js/app.nomodule.js": {
+    "version": "ca66c229",
+    "versionedUrl": "/jsenv-template-pwa/js/app.nomodule.js?v=ca66c229"
+  },
   "/jsenv-template-pwa/js/jsenv_service_worker.js": {
     "version": "350cbb84",
     "versionedUrl": "/jsenv-template-pwa/js/jsenv_service_worker.js?v=350cbb84"
+  },
+  "/jsenv-template-pwa/js/vendors.nomodule.js": {
+    "version": "076ab77a",
+    "versionedUrl": "/jsenv-template-pwa/js/vendors.nomodule.js?v=076ab77a"
+  },
+  "/jsenv-template-pwa/css/app.css": {
+    "version": "df3f7a57",
+    "versionedUrl": "/jsenv-template-pwa/css/app.css?v=df3f7a57"
   }
 };
 
 
-;(function(){var m = {"/jsenv-template-pwa/js/main.nomodule.js":"/jsenv-template-pwa/js/main.nomodule.js?v=814901e3","/jsenv-template-pwa/js/app.nomodule.js":"/jsenv-template-pwa/js/app.nomodule.js?v=658dcef8","/jsenv-template-pwa/css/app.css":"/jsenv-template-pwa/css/app.css?v=df3f7a57","/jsenv-template-pwa/js/vendors.nomodule.js":"/jsenv-template-pwa/js/vendors.nomodule.js?v=39bb75dc","/jsenv-template-pwa/other/roboto_v27_latin_regular.woff2":"/jsenv-template-pwa/other/roboto_v27_latin_regular.woff2?v=fda7bb73","/jsenv-template-pwa/other/logo.png":"/jsenv-template-pwa/other/logo.png?v=467b6542","/jsenv-template-pwa/js/jsenv_service_worker.js":"/jsenv-template-pwa/js/jsenv_service_worker.js?v=350cbb84"}; self.__v__ = function (s) { return m[s] || s }; })();
+;(function(){var m = {"/jsenv-template-pwa/js/main.nomodule.js":"/jsenv-template-pwa/js/main.nomodule.js?v=71f0c98e","/jsenv-template-pwa/js/vendors.nomodule.js":"/jsenv-template-pwa/js/vendors.nomodule.js?v=076ab77a","/jsenv-template-pwa/other/roboto_v27_latin_regular.woff2":"/jsenv-template-pwa/other/roboto_v27_latin_regular.woff2?v=fda7bb73","/jsenv-template-pwa/js/app.nomodule.js":"/jsenv-template-pwa/js/app.nomodule.js?v=ca66c229","/jsenv-template-pwa/css/app.css":"/jsenv-template-pwa/css/app.css?v=df3f7a57","/jsenv-template-pwa/other/logo.png":"/jsenv-template-pwa/other/logo.png?v=467b6542","/jsenv-template-pwa/js/jsenv_service_worker.js":"/jsenv-template-pwa/js/jsenv_service_worker.js?v=350cbb84"}; self.__v__ = function (s) { return m[s] || s }; })();
 
 self.importScripts(__v__("/jsenv-template-pwa/js/jsenv_service_worker.js")),self.__sw__.init({name:"product-name",logLevel:"info",resources:{"/jsenv-template-pwa/":{},...self.resourcesFromJsenvBuild||{}}});
